e50d274af2201582de348da6596090f561860eb2
erichensleyibm <eric.hensley@ibm.com>
2018-01-09 20:46:57 -0500

    survey

--- index.php
+++ index.php
@@ -1,111 +1,93 @@
-<!--
-/**
-* Copyright 2015 IBM Corp. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the “License”);
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* https://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an “AS IS” BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-->
 
-<?php include 'db.php';?>
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //if new message is being added
-    $cleaned_message = preg_replace('/[^a-zA-Z0-9.\s]/', '', $_POST["message"]); //remove invalid chars from input.
+// define variables and set to empty values
+$nameErr = $emailErr = $websiteErr = "";
+$name = $email = $comment = $website = "";
 
-    $strsq0 = "INSERT INTO MESSAGES_TABLE ( NAME) VALUES ('" . $cleaned_message .
-"');"; //query to insert new message
-    echo "<script type='text/javascript'>alert('$strsq0');</script>";
-
-    if ($mysqli->query($strsq0)) {
-        //echo "Insert success!";
-    } else {
-        echo "Cannot insert into the data table; check whether the table is created, or the database is active. "  . mysqli_error();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
     }
+  }
+  
+  if (empty($_POST["office"])) {
+    $officeErr = "Office is required";
+  } else {
+    $office = test_input($_POST["office"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($office, FILTER_VALIDATE_EMAIL)) {
+      $officeErr = "Invalid office format";
+    }
+  }
+    
+  if (empty($_POST["sector"])) {
+    $sector = "";
+  } else {
+    $sector = test_input($_POST["sector"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$sector)) {
+      $sectorErr = "Invalid Sector";
+    }
+  }
+
+  if (empty($_POST["project"])) {
+    $project = "";
+  } else {
+    $project = test_input($_POST["project"]);
+  }
+
 }
 
-//Query the DB for messages
-$strsql = "select * from MESSAGES_TABLE limit 100";
-if ($result = $mysqli->query($strsql)) {
-   // printf("<br>Select returned %d rows.\n", $result->num_rows);
-} else {
-        //Could be many reasons, but most likely the table isn't created yet. init.php will create the table.
-        echo "<b>Can't query the database, did you <a href = init.php>Create the table</a> yet?</b>";
-    }
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Office: <input type="text" name="office" value="<?php echo $office;?>">
+  <span class="error">* <?php echo $officeErr;?></span>
+  <br><br>
+  Sector: <input type="text" name="sector" value="<?php echo $sector;?>">
+  <span class="error"><?php echo $sectorErr;?></span>
+  <br><br>
+  Project: <textarea name="project" rows="5" cols="40"><?php echo $project;?></textarea>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
 
-<!DOCTYPE html>
-<html>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $office;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+?>
 
-<head>
-    <title>Internal CBD POC</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link rel="stylesheet" href="style.css" />
-</head>
-
-<body>
-    <div class="">
-        <img class="newappIcon" src="images/newapp-icon.png" />
-        <h1>
-					Welcome to the <span class="blue">Consulting By Degrees </span> Information Center. 
-				</h1>
-        <p class="description">This page will allow you to share your current project, view others' and foster networking.  <br>
-
-
-            <input type="button" class = "mybutton" onclick="window.location = 'init.php';" class="btn" value="(Re-)Create table"></input></p>
-            </br>
-
-    
-    <table id='notes' class='records'><tbody>
-        
-        <?php
-            echo "<tr>\n";
-            while ($property = mysqli_fetch_field($result)) {
-                    echo '<th>' .  $property->name . "</th>\n"; //the headings
-
-            }
-            echo "</tr>\n";
-
-            mysqli_data_seek ( $result, 0 );
-            if($result->num_rows == 0){ //nothing in the table
-                        echo '<td>Empty!</td>';
-            }
-                
-            while ( $row = mysqli_fetch_row ( $result ) ) {
-                echo "<tr>\n";
-                for($i = 0; $i < mysqli_num_fields ( $result ); $i ++) {
-                    echo '<td>' . "$row[$i]" . '</td>';
-                }
-                echo "</tr>\n";
-            }
-
-            $result->close();
-            mysqli_close();
-        ?>
-        <tr>
-            <form method = "POST"> <!--FORM: will submit to same page (index.php), and if ($_SERVER["REQUEST_METHOD"] == "POST") will catch it --> 
-                <td colspan = "2">
-                <input type = "text" style = "width:100%" name = "message" autofocus onchange="saveChange(this)" onkeydown="onKey(event)"></input>
-                </td>
-                <td>
-                    <button class = "mybutton" type = "submit">Add New Message</button></td></tr>
-                </td> 
-            </form>
-        </tr>
-        </tbody>
-    </table>
-    </div>
 </body>
-
 </html>
